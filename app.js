6e7852e4dd1518e67a7d09671b2a28af0414178b
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-05-20 22:28:50 +0300

    refactor: add lines

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const game = require("./controllers/gamecontroller");
 db.sync();
 
 app.use(express.json());
-app.use(express.urlencoded());
+
 app.use("/api/auth", user);
+
 app.use(require("./middleware/validate-session"));
+
 app.use("/api/game", game);
+
 app.listen(4000, function () {
     console.log("App is listening on 4000");
 });
